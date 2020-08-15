0670cf4499985eba7b80652e4b952cedbf2228a2
Matheus Pereira Almeida <matheus.pereira.almeida10@gmail.com>
2020-08-14 21:00:19 -0300

    Tela Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import './app.component.css';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  formulario: FormGroup; 
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+
+    this.formulario = this.formBuilder.group({
+      moedaDeOrigem: [null, Validators.required],
+      quantidadeDaMoedaDeOrigem: [null, Validators.required],
+      moedaDeDestino: [null, Validators.required],
+      quantidadeDaMoedaDeDestino: [null, Validators.required]
+    })
+  }
+
+  
+  onSubmit(){
+    
+    console.log(this.formulario);
+    /*
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value)).subscribe(dados => {
+      console.log(dados);
+      
+      //reseta o form
+      this.resetar();
+    },
+    (error: any) => alert("erro"));//com esse erro ele não reseta
+    */
+  }
+
+  resetar(){
+    this.formulario.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//components
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { SelectionComponent } from './selection/selection.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { SuccessComponent } from './success/success.component';
 
+//impor necessário pro dropdown
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule  }   from '@angular/forms';
+
+//import do http
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    SelectionComponent,
+    ConfirmationComponent,
+    SuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
